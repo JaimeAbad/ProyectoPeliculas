93fa7483e0113fc37723ac413cbdacb134a375a1
Jaime <aquileseeb@gmail.com>
2019-11-25 18:21:37 +0100

    c3
    haciendo el buscador de arriba

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PeliService } from 'src/app/services/peli.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  peliculas: any[] = [];
+  loading: boolean;
 
-  constructor() { }
+  constructor(private peli: PeliService) { }
 
   ngOnInit() {
+  }
+
+  buscar(termino: string) {
+    console.log(termino);
+
+    this.loading = true;
+    this.peli.getBusquedaPeliculas(termino).subscribe((data: any) => {
+      console.log(data);
+
+      this.peliculas = data;
+      this.loading = false;
+    });
   }
 
 }
